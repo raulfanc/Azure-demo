55772a1ccc89da20b8564ae596b926ec5d39822d
Rex Huang <raulfanc@gmail.com>
2023-02-15 19:47:33 +1300

    prior to run spark Job in databricks
    succesfully ran Spark Job to do Join and Transformations.

--- Terraform/Terraform.md
+++ Terraform/Terraform.md
@@ -1,14 +1,19 @@
-refer to: `https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/guides/azure_cli`
+# Terraform Docs
+Refer to [Terraform Docs](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs) for templates for Azure.
+
+## Useful commands
+`Terraform init` to initialise the Terraform folder
+
+`Terraform validate` to debug
+
+`Terraform apply` to apply code to infrustructure set up
+
+`Terraform destroy` to delete the applied infrustructure
 
 ## Logging into the Azure CLI
 Login to the Azure CLI using via terminal: `az login`, install Azure CLI if dont have it yet 
 Once logged in - list all Subscriptions via terminal: `az account list`
 Specify the Subscription to use via terminal (my demo id): `az account set --subscription="763c5c4a-0fc9-47a1-919f-3bee41db03a0"`
-
-## create 
-- Provider
-- Resource Group: # Create Resouce Group named "wsp_demo_RG"
-- Azure Storage account: # Create storage account (Data lake) named "datalakegen2wsp123"
 
 ## waiting for build
 1. Data Lake
@@ -21,5 +26,7 @@ Specify the Subscription to use via terminal (my demo id): `az account set --sub
 8. API management
 9. Azure Active Directory
 10. App Registration
+
+#
 
 
--- terraform/data_lake.tf
+++ terraform/data_lake.tf
@@ -4,53 +4,43 @@ resource "azurerm_resource_group" "wsp_demo_RG" {
   location = "east US"
 }
 
-# Storage Account (Data lake)
-resource "azurerm_storage_account" "datalakegen2wsp123" {
-  name                     = "datalakegen2wsp123"
-  resource_group_name      = azurerm_resource_group.wsp_demo_RG.name
-  location                 = azurerm_resource_group.wsp_demo_RG.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  account_kind             = "StorageV2"
-  is_hns_enabled           = "true"
-}
+# # Storage Account (Data lake)NFS 3.0 has to be enabled for Logic app to access
+# resource "azurerm_storage_account" "datalakegen2wsp123" {
+#   name                     = "datalakegen2wsp123"
+#   resource_group_name      = azurerm_resource_group.wsp_demo_RG.name
+#   location                 = azurerm_resource_group.wsp_demo_RG.location
+#   account_tier             = "Standard"
+#   account_replication_type = "LRS"
+#   account_kind             = "StorageV2"
+#   is_hns_enabled           = "true"
+# }
 
-resource "azurerm_storage_data_lake_gen2_filesystem" "datalakegen2wsp123" {
-  name                = "datalakegen2wsp123"
-  storage_account_id  = azurerm_storage_account.datalakegen2wsp123.id
+# resource "azurerm_storage_data_lake_gen2_filesystem" "datalakegen2wsp123" {
+#   name                = "datalakegen2wsp123"
+#   storage_account_id  = azurerm_storage_account.datalakegen2wsp123.id
+#   properties = {
+#     "Microsoft.Storage.DataLakeStore.Nfs.Enabled" = "True"
+#     "Microsoft.Storage.DataLakeStore.Nfs.Version" = "3.0"
+#   }
+# }
 
-  network_rule {
-    action = "Allow"
-    virtual_network_subnet_id = "<subnet_id>"
-  }
-
-  properties = {
-    "Microsoft.Storage.DataLakeStore.Nfs.Enabled" = "True"
-    "Microsoft.Storage.DataLakeStore.Nfs.Version" = "3.0"
-  } # NFS 3.0 has to be enabled for Logic app to access
-}
-
-# Event Hubs 
-resource "azurerm_resource_group" "wsp_demo_RG" {
-  name     = "wsp_demo_RG"
-  location = "East US"
-}
-
-resource "azurerm_eventhub_namespace" "eventhub-wsp-demo" {
-  name                = "eventhub-wsp-demo"
+#Event Hubs Namespace
+resource "azurerm_eventhub_namespace" "wspdemonamespace1" {
+  name                = "wspdemonamespace1"
   location            = azurerm_resource_group.wsp_demo_RG.location
   resource_group_name = azurerm_resource_group.wsp_demo_RG.name
-
-  sku {
-    name = "Standard"
-    tier = "Free"  #1GB free monthly
-  }
+  sku                 = "Basic"
+  capacity            = 1
+  zone_redundant      = false
+  
+  tags = {}
 }
 
-resource "azurerm_eventhub" "eventhub-wsp-demo" {
-  name                = "eventhub-wsp-demo"
-  namespace_name      = azurerm_eventhub_namespace.eventhub-wsp-demo.name
-  resource_group_name = azurerm_resource_group.eventhub-wsp-demo.name
-
-  partition_count = 2
+#event_hub
+resource "azurerm_eventhub" "eventhub_wsp_demo" {
+  name                = "eventhub_wsp_demo1"
+  namespace_name      = azurerm_eventhub_namespace.wspdemonamespace1.name
+  resource_group_name = "wsp_demo_RG"
+  partition_count     = 2
+  message_retention   = 1
 }
